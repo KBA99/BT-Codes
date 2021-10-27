2aeb78228ada765cd0261114c69cef5178d5162e
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-10-28 00:09:38 +0100

    Added useProxy package and imported into file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import puppeteer from "puppeteer-extra";
 import StealthPlugin from 'puppeteer-extra-plugin-stealth'
 import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker'
 import fs from 'fs'
+import useProxy from 'puppeteer-page-proxy'
 
 puppeteer.use(StealthPlugin())
 puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
