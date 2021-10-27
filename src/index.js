12626a560cb5951113fad60edee057d3d1bbf9e3
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-10-27 23:56:55 +0100

    Added implementation to close cookie banner

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
 const emails = "src/emails.txt";
 const successfullyEntered = "src/successfullyEntered.txt";
 
+const emailAddress = 'test@ukfastprime.com'
+
+const btPage = {
+    emailAddress: "#EmailAddress",
+    submit: "#contentbody > div > div > form > div.button-bar.bottom.float-right > button",
+    cookieBanner: '#spanAboutCookiesOk > button'
+}
+
 let successEnter = (email) => {
     fs.appendFile(successfullyEntered, `${email}}\n`, function (err) {
         if (err) console.log(err);
@@ -31,49 +39,67 @@ try {
 
 
 
+let start = async () => {
 
-// Add adblocker plugin to block all ads and trackers (saves bandwidth)
-// That's it, the rest is puppeteer usage as normal 😊
-    console.time('Request time:')
-    puppeteer.launch({ headless: false })
-    .then(async browser => {
+            console.log("==> Starting Browser")
 
-    const page = await browser.newPage()
-    await page.setViewport({ width: 800, height: 600 })
-    const navigationPromise = page.waitForNavigation()
+    // Add adblocker plugin to block all ads and trackers (saves bandwidth)
+    // That's it, the rest is puppeteer usage as normal 😊
+        console.time('Request time:')
 
-
-  /* Tests
-    console.log(`Testing adblocker plugin..`)
-    await page.goto('https://www.vanityfair.com')
-    await page.waitForTimeout(1000)
-    await page.screenshot({ path: 'adblocker.png', fullPage: true })
-
-    console.log(`Testing the stealth plugin..`)
-    await page.goto('https://bot.sannysoft.com')
-    await page.waitForTimeout(5000)
-    await page.screenshot({ path: 'stealth.png', fullPage: true })
-
-//   console.log(`All done, check the screenshots. ✨`)
-*/
+        const browser = await puppeteer.launch({headless: false, slowMo: 50})
 
 
-    await page.goto('https://shop.bt.com/forms/playstation-5');
-
-    // await page.waitForSelector('.modalContainer > .innerContainer > .container > #spanAboutCookiesOk > .button2');
-
-    // await page.click('.modalContainer > .innerContainer > .container > #spanAboutCookiesOk > .button2')
-
-    // await page.waitForSelector('#btnCookiePreference')
-    // await page.click('#btnCookiePreference')
+        const page = await browser.newPage()
+        await page.setViewport({ width: 800, height: 600 })
+        const navigationPromise = page.waitForNavigation()
 
 
-    // cookieModal = await page.$('#cookieModal')
+    /* Tests
+        console.log(`Testing adblocker plugin..`)
+        await page.goto('https://www.vanityfair.com')
+        await page.waitForTimeout(1000)
+        await page.screenshot({ path: 'adblocker.png', fullPage: true })
 
-    
+        console.log(`Testing the stealth plugin..`)
+        await page.goto('https://bot.sannysoft.com')
+        await page.waitForTimeout(5000)
+        await page.screenshot({ path: 'stealth.png', fullPage: true })
 
-    console.timeEnd('Request time:')
+    //   console.log(`All done, check the screenshots. ✨`)
+    */
 
-    // await browser.close()
+    console.log("==> Navigating to Page")
 
-})
+        await page.goto('https://shop.bt.com/forms/playstation-5');
+
+        // await page.waitForSelector('.modalContainer > .innerContainer > .container > #spanAboutCookiesOk > .button2');
+
+        // await page.click('.modalContainer > .innerContainer > .container > #spanAboutCookiesOk > .button2')
+
+        // await page.waitForSelector('#btnCookiePreference')
+        // await page.click('#btnCookiePreference')
+
+
+        // cookieModal = await page.$('#cookieModal')
+
+        console.log("==> Waiting for cookie selector")
+
+
+        await page.waitForSelector('.button2')
+        // await page.click('.button2')
+
+        console.log("==> Found cookie selector")
+
+        await page.click(btPage.cookieBanner)
+
+
+
+        console.timeEnd('Request time:')
+
+        // await browser.close()
+
+    }
+
+
+start()
